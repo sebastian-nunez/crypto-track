d4d8aa28a4ca89b598e474b9da064d7fb9cc2425
sebastian-nunez <02snunez@gmail.com>
2023-12-04 11:52:33 -0500

    Merge branch 'main' of https://github.com/sebastian-nunez/crypto-track into main

---
+++ crypto-track/Core/Home/Views/TopMoversItemView.swift
@@ -0,0 +1,48 @@
+//
+//  TopMoversItemView.swift
+//  crypto-track
+//
+//  Created by Sebastian on 12/4/23.
+//
+
+import SwiftUI
+
+struct TopMoversItemView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            // image
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+
+            // coin info
+            HStack {
+                Text("BTC")
+                    .font(.caption)
+                    .fontWeight(.bold)
+
+                Text("$20,330.00")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+
+            // coin % change
+            Text("+5.60%") // TODO: updated +/- sign based on price
+                .font(.title2)
+                .foregroundColor(.green) // TODO: update color based on -/+
+        }
+        .frame(width: 140, height: 140)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.systemGray4), lineWidth: 1)
+        )
+    }
+}
+
+struct TopMoversItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversItemView()
+    }
+}
---
+++ crypto-track/Core/Home/Views/TopMoversView.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoversView.swift
+//  crypto-track
+//
+//  Created by Sebastian on 12/4/23.
+//
+
+import SwiftUI
+
+struct TopMoversView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoversView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversView()
+    }
+}
