3952d184d583e1ab71d650b79cb2ee04eada5867
sebastian-nunez <02snunez@gmail.com>
2023-12-04 10:17:31 -0500

    Initial Commit

---
+++ crypto-track/crypto_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_trackApp.swift
+//  crypto-track
+//
+//  Created by Sebastian on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct crypto_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
