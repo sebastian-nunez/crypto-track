e15ffd59338f603885abc586348c0a011a927aa0
sebastian-nunez <02snunez@gmail.com>
2023-12-04 11:34:24 -0500

    initial commit

---
+++ crypto-track/Core/Home/Views/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  crypto-track
+//
+//  Created by Sebastian on 12/4/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                // top movers view
+                
+                // all coins view
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
