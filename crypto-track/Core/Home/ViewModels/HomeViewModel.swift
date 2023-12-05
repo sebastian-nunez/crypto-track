60dd76efe8e65dee24f9e8c24b0430d8be187c25
sebastian-nunez <02snunez@gmail.com>
2023-12-05 00:54:32 -0500

    Add topMovingCoins property and configureTopMovingCoins method

--- crypto-track/Core/Home/ViewModels/HomeViewModel.swift
+++ crypto-track/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,7 +10,10 @@ import SwiftUI
 class HomeViewModel: ObservableObject {
     static let FETCH_ALL_COIN_INFO_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en&precision=2&api_key=CG-m829evJneyQyubEVmPCSLyv2" // TODO: please don't take my API key :/
 
+    static let NUM_MAX_TOP_MOVING_COINS_RESULTS = 10
+
     @Published var coins = [Coin]()
+    @Published var topMovingCoins = [Coin]()
 
     init() {
         fetchCoinData()
@@ -44,11 +47,22 @@ class HomeViewModel: ObservableObject {
                 let coins = try JSONDecoder().decode([Coin].self, from: data)
                 print("DEBUG: Coins \(coins)")
 
-                self.coins = coins
+                DispatchQueue.main.async {
+                    self.coins = coins
+                    self.configureTopMovingCoins()
+                }
             } catch {
                 print("DEBUG: Failed to decode coins \(error)")
             }
 
         }.resume()
+    }
+
+    func configureTopMovingCoins() {
+        let topMovers = coins.sorted(by: { coin1, coin2 in
+            coin1.priceChangePercentage24H > coin2.priceChangePercentage24H
+        })
+
+        topMovingCoins = Array(topMovers.prefix(Self.NUM_MAX_TOP_MOVING_COINS_RESULTS))
     }
 }
